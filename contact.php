ca6cfbf8976ac9d9eb639bd1abaa195b4536a299
Sumeet Masih <billu.pillu123@gmail.com>
2016-02-13 15:13:16 +0530

    beta commit

--- contact.php
+++ contact.php
@@ -3,7 +3,7 @@ $field_name = $_POST['cf_name'];
 $field_email = $_POST['cf_email'];
 $field_message = $_POST['cf_message'];
 
-$mail_to = 'newthinks@live.com';
+$mail_to = 'sumeet@appsclay.com';
 $subject = 'Message from a site visitor '.$field_name;
 
 $body_message = 'From: '.$field_name."\n";
@@ -18,14 +18,14 @@ $mail_status = mail($mail_to, $subject, $body_message, $headers);
 if ($mail_status) { ?>
 	<script language="javascript" type="text/javascript">
 		alert('Thank you for the message. We will contact you shortly.');
-		window.location = 'index.html';
+		window.location = 'index.php';
 	</script>
 <?php
 }
 else { ?>
 	<script language="javascript" type="text/javascript">
 		alert('Message failed. Please, send an email to newthinks@live.com');
-		window.location = 'index.html';
+		window.location = 'index.php';
 	</script>
 <?php
 }
